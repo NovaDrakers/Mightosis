3a118efb0995790886a23ccc57bcf7f33819d4a8
mysterymisery <91627134+mysterymisery@users.noreply.github.com>
2023-11-24 22:31:17 -0500

    dynamic paneling/golgi
    added dynamic panel switching between the nucleus and golgi, as well as added buttons to create troops and store number of created troops

--- Assets/Script/GolgiScript.cs
+++ Assets/Script/GolgiScript.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GolgiScript : MonoBehaviour
 {
+    GameObject Panel;
+    Canvas canvas;
+
+    public TextMeshProUGUI ProteinText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Panel = GameObject.Find("GolgiPanel");
+        canvas = GameObject.FindAnyObjectByType<Canvas>(); 
+
+
+        Panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+
+    private void OnMouseDown()
+    {
+        if (!Panel.activeSelf)
+        {
+            Transform canvasTransform = canvas.transform;
+
+            // Iterate through all the children of the Canvas
+            for (int i = 0; i < canvasTransform.childCount; i++)
+            {
+                // Access each child using the getChild method
+                Transform child = canvasTransform.GetChild(i);
+
+                child.gameObject.SetActive(false);
+            }
+            Panel.SetActive(true);
+            Panel.SetActive(true);
+        }
+    }
+
+    
 }
