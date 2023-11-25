d16453c8403dd3303dd0f0c37626f06cefaf3de1
mysterymisery <91627134+mysterymisery@users.noreply.github.com>
2023-11-25 16:57:15 -0500

    shield added, golgi spawns troops

--- Assets/Script/PanelButtons.cs
+++ Assets/Script/PanelButtons.cs
@@ -9,11 +9,17 @@ using UnityEngine.UI;
 public class PanelButtons : MonoBehaviour
 {
     public GameObject Builder;
+    public GameObject Ranged;
+    public GameObject Melee;
+    public GameObject Tank;
+
     public GameObject Nucleus;
+    public GameObject Golgi;
 
     public int ranged = 0;
     public int melee = 0;
     public int tank = 0;
+    float spawnDistance = 1f;
 
 
     // Start is called before the first frame update
@@ -30,7 +36,8 @@ public class PanelButtons : MonoBehaviour
 
     public void CreateWorker()
     {
-        float spawnDistance = 1f;
+        Nucleus = GameObject.FindGameObjectWithTag("Nucleus");
+        
         
         if (Builder == null)
         {
@@ -79,8 +86,46 @@ public class PanelButtons : MonoBehaviour
         Debug.Log("" + ranged +", " + melee + ", " + tank);
     }
 
-    public void SpawnTroop()
+    public void SpawnTroop(string troop)
     {
+        Golgi = GameObject.FindGameObjectWithTag("Golgi");
+        float randomAngle = Random.Range(1f, 2f * Mathf.PI);
+        float xOffset = spawnDistance * Mathf.Cos(randomAngle);
+        float zOffset = spawnDistance * Mathf.Sin(randomAngle);
+
+        switch (troop)
+        {
+            case "Ranged":
+
+                if (ranged > 0)
+                {
+                   Instantiate(Ranged, new Vector3(Golgi.transform.position.x + xOffset, 0f, Golgi.transform.position.z + zOffset), transform.rotation);
+                   ranged--;
+                }
+                break;
+
+            case "Melee":
+
+                if (melee > 0)
+                {
+                    Instantiate(Melee, new Vector3(Golgi.transform.position.x + xOffset, 0f, Golgi.transform.position.z + zOffset), transform.rotation);
+                    melee--;
+                }
+                break;
+
+            case "Tank":
+
+                if (tank > 0)
+                {
+                    Instantiate(Tank, new Vector3(Golgi.transform.position.x + xOffset, 0f, Golgi.transform.position.z + zOffset), transform.rotation);
+                    tank--;
+                }
+                break;
+
+            default:
+
+                break;
+        }
 
     }
 }
